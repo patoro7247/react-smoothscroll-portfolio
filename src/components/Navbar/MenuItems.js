058dcd56431754f45ad1260b29f9abea940053ab
Patrick Harding <patrickhardingr@gmail.com>
2022-02-24 22:40:31 -0800

    navbar with gradient

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'About',
+        url : '#',
+        cName : 'nav-links'
+    },
+    {
+        title : 'Projects',
+        url : '#',
+        cName : 'nav-links'
+    },
+    
+]
